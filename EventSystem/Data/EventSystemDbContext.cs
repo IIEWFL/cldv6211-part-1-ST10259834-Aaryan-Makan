49af9304f3be8994da09809c56295551aab74999
Aaryan Makan <st10259834@vcconnect.edu.za>
2025-04-01 00:29:59 +0200

    published

---
+++ EventSystem/Data/EventSystemDbContext.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using EventSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventSystem.Data;
+
+public partial class EventSystemDbContext : DbContext
+{
+    public EventSystemDbContext()
+    {
+    }
+
+    public EventSystemDbContext(DbContextOptions<EventSystemDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Booking> Bookings { get; set; }
+
+    public virtual DbSet<Event> Events { get; set; }
+
+    public virtual DbSet<Venue> Venues { get; set; }
+
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Booking>(entity =>
+        {
+            entity.HasKey(e => e.BookingId).HasName("PK__Booking__73951ACD20C449AE");
+
+            entity.ToTable("Booking");
+
+            entity.Property(e => e.BookingId).HasColumnName("BookingID");
+            entity.Property(e => e.EventId).HasColumnName("EventID");
+            entity.Property(e => e.VenueId).HasColumnName("VenueID");
+
+            entity.HasOne(d => d.Event).WithMany(p => p.Bookings)
+                .HasForeignKey(d => d.EventId)
+                .HasConstraintName("FK__Booking__EventID__3D5E1FD2");
+
+            entity.HasOne(d => d.Venue).WithMany(p => p.Bookings)
+                .HasForeignKey(d => d.VenueId)
+                .HasConstraintName("FK__Booking__VenueID__3C69FB99");
+        });
+
+        modelBuilder.Entity<Event>(entity =>
+        {
+            entity.HasKey(e => e.EventId).HasName("PK__Event__7944C8703BABF94A");
+
+            entity.ToTable("Event");
+
+            entity.Property(e => e.EventId).HasColumnName("EventID");
+            entity.Property(e => e.Description)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.EventName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.VenueId).HasColumnName("VenueID");
+
+            entity.HasOne(d => d.Venue).WithMany(p => p.Events)
+                .HasForeignKey(d => d.VenueId)
+                .HasConstraintName("FK__Event__VenueID__398D8EEE");
+        });
+
+        modelBuilder.Entity<Venue>(entity =>
+        {
+            entity.HasKey(e => e.VenueId).HasName("PK__Venue__3C57E5D2AFDE9C28");
+
+            entity.ToTable("Venue");
+
+            entity.Property(e => e.VenueId).HasColumnName("VenueID");
+            entity.Property(e => e.ImageUrl)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Location)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.VenueName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
